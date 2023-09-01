a40af60f26866829c2d2fb4708fdeecf1f2f2c7a
Robert <ro13ertnichols@gmail.com>
2023-09-01 20:11:14 +0930

    added collisions

--- utils.js
+++ utils.js
@@ -2,4 +2,35 @@ const utils = {
   withGrid(n) {
     return n * 16;
   },
+
+  asGridCoord(x, y) {
+    return `${x * 16},${y * 16}`;
+  },
+
+  nextPosition(initialX, initialY, direction) {
+    let x = initialX;
+    let y = initialY;
+    const tileSize = 16;
+    switch (direction) {
+      case "up": {
+        y -= tileSize;
+        break;
+      }
+      case "down": {
+        y += tileSize;
+        break;
+      }
+      case "left": {
+        x -= tileSize;
+        break;
+      }
+      case "right": {
+        x += tileSize;
+        break;
+      }
+      default:
+        break;
+    }
+    return { x, y };
+  },
 };
